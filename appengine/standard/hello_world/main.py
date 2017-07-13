ba6a9f06ef79552e2f8a35e274d4b8f5835a5b9c
Bonnie <kbonnie@gmail.com>
2017-07-12 21:02:50 -0700

    added slips, working

--- appengine/standard/hello_world/main.py
+++ appengine/standard/hello_world/main.py
@@ -2,6 +2,10 @@ from google.appengine.ext import ndb
 import webapp2
 import json
 
+class Slip(ndb.Model):
+    number = ndb.IntegerProperty()
+    arrival_date = ndb.DateProperty()
+
 
 class Boat(ndb.Model):
     name = ndb.StringProperty(required=True)
@@ -13,6 +17,26 @@ class Boat(ndb.Model):
 class Fish(ndb.Model):
     name = ndb.StringProperty(required=True)
     numFriends = ndb.IntegerProperty()
+
+
+class SlipHandler(webapp2.RequestHandler):
+    def post(self):
+        slip_data = json.loads(self.request.body)
+        new_slip = Slip(number=slip_data['number'])
+        new_slip.put()
+        slip_dict = new_slip.to_dict()
+        slip_dict['self'] = '/slip/' + new_slip.key.urlsafe()
+        self.response.write(json.dumps(slip_dict))
+
+
+    def get(self, id=None):
+        if id:
+            s = ndb.Key(urlsafe=id).get()
+            s_d = s.to_dict()
+            s_d['self'] = "/slip/" + id
+            self.response.write(json.dumps(s_d))
+        else:
+            self.response.write("There are no slips")
 
 class BoatHandler(webapp2.RequestHandler):
     def post(self):
@@ -72,4 +96,6 @@ app = webapp2.WSGIApplication([
     ('/fish/(.*)', FishHandler),
     ('/boat', BoatHandler),
     ('/boat/(.*)', BoatHandler),
+    ('/slip', SlipHandler),
+    ('/slip/(.*)', SlipHandler),
 ], debug=True)
