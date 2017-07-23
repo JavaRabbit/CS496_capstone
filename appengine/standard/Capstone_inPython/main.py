fc6a292967a2aa395971adb01bb4f2c68a943bd2
Bonnie <kbonnie@gmail.com>
2017-07-23 01:01:21 -0700

    user can be created

--- appengine/standard/Capstone_inPython/main.py
+++ appengine/standard/Capstone_inPython/main.py
@@ -7,12 +7,15 @@ import jinja2
 JINJA_ENV = jinja2.Environment(
  loader = jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
-'''
-class Boat(ndb.Model):
+class User(ndb.Model):
     name = ndb.StringProperty(required=True)
-    type = ndb.StringProperty()
-    length = ndb.IntegerProperty()
-    at_sea = ndb.BooleanProperty(default=True)
+    username = ndb.StringProperty(required=True)
+    password = ndb.StringProperty(required=True)
+    email = ndb.StringProperty()
+    signature = ndb.IntegerProperty()
+
+
+'''
 
 class Slip(ndb.Model):
     number = ndb.IntegerProperty(required=True)
@@ -26,8 +29,8 @@ class Fish(ndb.Model):
     numFriends = ndb.IntegerProperty()
 
 
-
 '''
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -38,13 +41,50 @@ class MainPage(webapp2.RequestHandler):
         # controller tells to render the index.html template
         self.response.out.write(template.render())
 
+class CreateUser(webapp2.RequestHandler):
+    def get(self):
+        # set the view to be index.html found in folder templates
+        template = JINJA_ENV.get_template('createUser.html')
+        # controller tells to render the index.html template
+        self.response.out.write(template.render())
 
+    def post(self):
+        #new_user = User(name="alpha", username = "alpha_username", password = "password")
+        username = self.request.get("username")
+        realname = self.request.get("realname")
+        password = self.request.get("password")
+        email = self.request.get("email")
+        new_user = User(name=realname, username = username, password = password, email = email)
+        new_user.put() # put it into the ndb store
+
+        # for now, route to all users
+        self.redirect("/users")
+
+# for debugging purposes only
+class UserHandler(webapp2.RequestHandler):
+    def get(self, id=None):
+        #r get all users
+        get_user_query_results = [get_user_query.to_dict()
+                                      for get_user_query in User.query()]
+        self.response.write(json.dumps(get_user_query_results))  # display results to user
+
+class SignIn(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENV.get_template('signIn.html')
+        # controller tells to render the index.html template
+        self.response.out.write(template.render())
+
+    #def post(self):
+        # verify that username and password are correct
 
 app = webapp2.WSGIApplication([
 
     #('/fish', FishHandler),
     #('/fish/(.*)', FishHandler),
 
-    ('/', MainPage)
+    ('/', MainPage),
+    ('/createUser', CreateUser),
+    ('/signIn', SignIn),
+    ('/users', UserHandler)
 
 ], debug=True)
